0abb7cbba673a14dffc542e50a88304d7320c346
Theeraphol Wattanavekin <theeraphol.wat@gmail.com>
2015-10-03 15:18:27 +0700

    Add back header to raw data

--- src/js/main-a.js
+++ src/js/main-a.js
@@ -4,5 +4,8 @@ $(function() {
   d3.select("body")
     .append("p")
     .text("New paragraph!");
+  d3.csv("/public/data/small_samplers_newyear_casualties.csv", function(data) {
+    console.log(data);
+  });
 });
 
